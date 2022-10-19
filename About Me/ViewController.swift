3830c90ae573b06bebd21bcfac65d065f02dc6e4
Rifai Su <rifaisu@gmail.com>
2022-10-19 19:46:40 +0800

    Initial Commit

---
+++ About Me/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  About Me
+//
+//  Created by Rifai Su on 19/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
